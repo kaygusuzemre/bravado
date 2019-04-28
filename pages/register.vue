2124e152df180227a152678b242984da9e74c7bc
Efe Ali GULOCAK <aliefegulocak@gmail.com>
2019-04-28 13:50:48 +0300

    register

--- pages/register.vue
+++ pages/register.vue
@@ -1,48 +1,25 @@
 <template>
     <div>
-        <h1>Register Page</h1>
-    
-    
-    <b-form @submit="onSubmit" @reset="onReset" v-if="show">
-      <b-form-group
-        id="input-group-1"
-        label="Email address:"
-        label-for="input-1"
-        description="We'll never share your email with anyone else."
-      >
-        <b-form-input
-          id="input-1"
-          v-model="form.email"
-          type="email"
-          required
-          placeholder="Enter email"
-        ></b-form-input>
-      </b-form-group>
+<b-container id="registerform">
+<p id="mainheader">BRAVADO Registeration</p>
+<b-card border-variant="dark" >
+  <p id="header">Start with your Social Account</p>
+  <div class="text-center">
+  <b-button-group vertical>
+<b-button href="" size="lg" variant="primary">  Login via Facebook</b-button>
+<b-button href="" size="lg" variant="danger">  Login via Gmail</b-button>
+</b-button-group>
+</div>
+<p id="divider">OR</p>
+   <div>
+    <b-form-input v-model="text" placeholder="Enter your name"></b-form-input>
+    <div class="mt-2">Value: {{ text }}</div>
+  </div>
 
-      <b-form-group id="input-group-2" label="Your Name:" label-for="input-2">
-        <b-form-input
-          id="input-2"
-          v-model="form.name"
-          required
-          placeholder="Enter name"
-        ></b-form-input>
-      </b-form-group>
 
-      <b-form-group id="input-group-3" label="Food:" label-for="input-3">
-        <b-form-select
-          id="input-3"
-          v-model="form.food"
-          :options="foods"
-          required
-        ></b-form-select>
-      </b-form-group>
 
-      <b-form-group id="input-group-4">
-        <b-form-checkbox-group v-model="form.checked" id="checkboxes-4">
-          <b-form-checkbox value="me">Check me out</b-form-checkbox>
-          <b-form-checkbox value="that">Check that out</b-form-checkbox>
-        </b-form-checkbox-group>
-      </b-form-group>
+
+
 
       <b-button type="submit" variant="primary">Submit</b-button>
       <b-button type="reset" variant="danger">Reset</b-button>
