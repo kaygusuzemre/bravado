63fe253fb2104a5c8d135a47ee736f8a5bc737f2
Davut KARA <davutkara.comtr@yandex.com>
2019-04-28 14:55:18 +0300

    fix: register page gird system

--- pages/register.vue
+++ pages/register.vue
@@ -1,122 +1,153 @@
 <template>
+  <div>
+    <b-container id="login-register">
+      <p id="mainheader">BRAVADO Registeration</p>
+      <nuxt-link to="/">Homepage</nuxt-link>
+      <b-row>
+        <b-col md="6">
+          <div id="socialdiv">
+            <p id="header" class="text-center">LOGIN FORM</p>
 
-   <div >
-<b-container id="registerform">
-<p id="mainheader">BRAVADO Registeration</p>
-<b-card border-variant="dark" >
-  <div id="socialdiv" class="text-center">
-  <p id="header">Start with your Social Account</p>
-  
-  <b-button-group vertical>
-<b-button href="" size="lg" variant="primary">  Login via Facebook</b-button>
-<b-button href="" size="lg" variant="danger">  Login via Gmail</b-button>
-</b-button-group>
-</div>
+            <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+              <b-form-group id="input-group-1" label="Email address:" label-for="input-1">
+                <b-form-input
+                  id="input-1"
+                  v-model="form.email"
+                  type="email"
+                  required
+                  placeholder="Enter email"
+                ></b-form-input>
+              </b-form-group>
 
+              <b-form-group id="input-group-3" label="Password:" label-for="input-3">
+                <b-form-input
+                  id="input-3"
+                  v-model="form.Password"
+                  type="password"
+                  required
+                  placeholder="Password"
+                ></b-form-input>
+              </b-form-group>
 
-   <div id="registerform">
-  
+              <b-button type="submit" size="lg" variant="primary">Login via account</b-button>
 
-  
-    <h1>Register Page</h1>
+              <b-button href size="lg" variant="danger">Login via Gmail</b-button>
+            </b-form>
+          </div>
+        </b-col>
+        <b-col md="6">
+          <div id="registerform">
+            <p id="header" class="text-center">REGISTER FORM</p>
 
-    <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+            <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+              <b-form-group
+                id="input-group-1"
+                label="Email address:"
+                label-for="input-1"
+                description="We'll never share your email with anyone else."
+              >
+                <b-form-input
+                  id="input-1"
+                  v-model="form.email"
+                  type="email"
+                  required
+                  placeholder="Enter email"
+                ></b-form-input>
+              </b-form-group>
 
-      <b-form-group
-        id="input-group-1"
-        label="Email address:"
-        label-for="input-1"
-        description="We'll never share your email with anyone else."
-      >
-        <b-form-input
-          id="input-1"
-          v-model="form.email"
-          type="email"
-          required
-          placeholder="Enter email"
-        ></b-form-input>
-      </b-form-group>
+              <b-form-group id="input-group-2" label="Your Name:" label-for="input-2">
+                <b-form-input id="input-2" v-model="form.name" required placeholder="Enter name"></b-form-input>
+              </b-form-group>
 
-      <b-form-group id="input-group-2" label="Your Name:" label-for="input-2">
-        <b-form-input id="input-2" v-model="form.name" required placeholder="Enter name"></b-form-input>
-      </b-form-group>
+              <b-form-group id="input-group-2" label="Your Surname:" label-for="input-2">
+                <b-form-input id="input-2" v-model="form.name" required placeholder="Enter surname"></b-form-input>
+              </b-form-group>
 
-      <b-form-group id="input-group-3" label="Password:" label-for="input-3">
-        <b-form-input
-          id="input-3"
-          v-model="form.Password"
-          type='password'
-          required
-          placeholder="Enter name"
-        ></b-form-input>
-      </b-form-group>
+              <b-form-group id="input-group-2" label="Your Age:" label-for="input-2">
+                <b-form-input
+                  type="number"
+                  id="input-2"
+                  v-model="form.name"
+                  required
+                  placeholder="Enter age"
+                ></b-form-input>
+              </b-form-group>
 
-      
+              <b-form-group label="Your gender">
+                <b-form-radio v-model="selected" name="some-radios" value="A">Male</b-form-radio>
+                <b-form-radio v-model="selected" name="some-radios" value="B">Female</b-form-radio>
+              </b-form-group>
 
-      <b-button type="submit" variant="primary">Submit</b-button>
-      <b-button type="reset" variant="danger">Reset</b-button>
-    </b-form>
-    
-    
+              <b-form-group id="input-group-3" label="Password:" label-for="input-3">
+                <b-form-input
+                  id="input-3"
+                  v-model="form.Password"
+                  type="password"
+                  required
+                  placeholder="Enter name"
+                ></b-form-input>
+              </b-form-group>
+
+              <b-form-group id="input-group-3" label="Password gender:" label-for="input-3">
+                <b-form-input
+                  id="input-3"
+                  v-model="form.Password"
+                  type="password"
+                  required
+                  placeholder="Password gender"
+                ></b-form-input>
+              </b-form-group>
+
+              <b-button type="submit" variant="primary">Submit</b-button>
+              <b-button type="reset" variant="danger">Reset</b-button>
+            </b-form>
+          </div>
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
-  
-</b-card>
-
-
-
-
-
-
-</b-container>
-
-
-
-
-   </div>
 </template>
 
 <script>
 export default {
-    data() {
-      return {
-        form: {
-          email: '',
-          name: '',
-          password: null,
-          checked: []
-        },
-        
-        show: true
-      }
-    },
-   
+  data() {
+    return {
+      form: {
+        email: '',
+        name: '',
+        password: null,
+        checked: []
+      },
+
+      show: true
+    }
   }
-  
+}
 </script>
 
 <style>
-#mainheader{
-  font-size:50px;
-  text-align:center;
+body {
+  background: #d9e9e9;
+}
+
+#mainheader {
+  font-size: 50px;
+  text-align: center;
   background-color: lightblue;
   color: black;
   padding: 40px;
 }
 
-#header{
-  font-size:30px;
-  text-align:center;
-  
+#login-register {
+  background: white;
+  padding: 10px;
+}
+
+#header {
+  font-size: 30px;
+  text-align: center;
+
   color: black;
   padding: 40px;
-}
-#socialdiv{
-background-color:rgb(177, 171, 90);
-padding:30px;
-margin-bottom:40px;
-}
-#registerform{
-
-
 }
 </style>
