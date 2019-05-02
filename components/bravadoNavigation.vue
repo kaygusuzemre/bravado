6f6263bba9f93413cca7061d7d442a562a6e4ac5
Davut KARA <davutkara.comtr@yandex.com>
2019-05-02 23:50:55 +0300

    add: navigation component

---
+++ components/bravadoNavigation.vue
@@ -0,0 +1,31 @@
+<template>
+  <b-nav id="navigation" tabs justified align="center">
+    <b-nav-item active>
+      <b-img src="/icons/svg/009-teamwork.svg" fluid/>
+      <hr>
+      <span>Dashboard</span>
+    </b-nav-item>
+    <b-nav-item>
+      <b-img src="/icons/svg/006-target.svg" fluid/>
+      <hr>
+      <span>My profile</span>
+    </b-nav-item>
+    <b-nav-item>
+      <b-img src="/icons/svg/022-conversation.svg" fluid/>
+      <hr>
+      <span>All challanges</span>
+    </b-nav-item>
+    <b-nav-item disabled>
+      <b-img src="/icons/svg/036-employee-2.svg" fluid/>
+      <hr>
+      <span>Log out</span>
+    </b-nav-item>
+  </b-nav>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
