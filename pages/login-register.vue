527ec2468790477b915b96647ee1326fa13cdbc1
Davut KARA <davutkara.comtr@yandex.com>
2019-05-02 16:33:32 +0300

    fix(login-register): fix variable names

--- pages/login-register.vue
+++ pages/login-register.vue
@@ -9,9 +9,9 @@
             <p id="header" class="text-center">LOGIN FORM</p>
 
             <b-form @submit="onSubmit" @reset="onReset" v-if="show">
-              <b-form-group id="input-group-1" label="Email address:" label-for="input-1">
+              <b-form-group id="input-group-1" label="Email address:" label-for="input-email">
                 <b-form-input
-                  id="input-1"
+                  id="input-email"
                   v-model="form.email"
                   type="email"
                   required
@@ -19,9 +19,9 @@
                 ></b-form-input>
               </b-form-group>
 
-              <b-form-group id="input-group-3" label="Password:" label-for="input-3">
+              <b-form-group id="input-group-3" label="Password:" label-for="input-pw">
                 <b-form-input
-                  id="input-3"
+                  id="input-pw"
                   v-model="form.Password"
                   type="password"
                   required
@@ -43,11 +43,11 @@
               <b-form-group
                 id="input-group-1"
                 label="Email address:"
-                label-for="input-1"
+                label-for="input-email-register"
                 description="We'll never share your email with anyone else."
               >
                 <b-form-input
-                  id="input-1"
+                  id="input-email-register"
                   v-model="form.email"
                   type="email"
                   required
@@ -55,32 +55,37 @@
                 ></b-form-input>
               </b-form-group>
 
-              <b-form-group id="input-group-2" label="Your Name:" label-for="input-2">
-                <b-form-input id="input-2" v-model="form.name" required placeholder="Enter name"></b-form-input>
+              <b-form-group id="input-group-2" label="Your Name:" label-for="input-name">
+                <b-form-input id="input-name" v-model="form.name" required placeholder="Enter name"></b-form-input>
               </b-form-group>
 
-              <b-form-group id="input-group-2" label="Your Surname:" label-for="input-2">
-                <b-form-input id="input-2" v-model="form.name" required placeholder="Enter surname"></b-form-input>
+              <b-form-group id="input-group-2" label="Your Surname:" label-for="input-surName">
+                <b-form-input
+                  id="input-surName"
+                  v-model="form.name"
+                  required
+                  placeholder="Enter surname"
+                ></b-form-input>
               </b-form-group>
 
-              <b-form-group id="input-group-2" label="Your Age:" label-for="input-2">
+              <b-form-group id="input-group-2" label="Your Age:" label-for="input-age">
                 <b-form-input
                   type="number"
-                  id="input-2"
-                  v-model="form.name"
+                  id="input-age"
+                  v-model="form.age"
                   required
                   placeholder="Enter age"
                 ></b-form-input>
               </b-form-group>
 
               <b-form-group label="Your gender">
-                <b-form-radio v-model="selected" name="some-radios" value="A">Male</b-form-radio>
-                <b-form-radio v-model="selected" name="some-radios" value="B">Female</b-form-radio>
+                <b-form-radio v-model="form.gender" name="some-radios" value="A">Male</b-form-radio>
+                <b-form-radio v-model="form.gender" name="some-radios" value="B">Female</b-form-radio>
               </b-form-group>
 
-              <b-form-group id="input-group-3" label="Password:" label-for="input-3">
+              <b-form-group id="input-group-3" label="Password:" label-for="input-pw1">
                 <b-form-input
-                  id="input-3"
+                  id="input-pw1"
                   v-model="form.Password"
                   type="password"
                   required
@@ -88,9 +93,9 @@
                 ></b-form-input>
               </b-form-group>
 
-              <b-form-group id="input-group-3" label="Password gender:" label-for="input-3">
+              <b-form-group id="input-group-3" label="Password gender:" label-for="input-pw2">
                 <b-form-input
-                  id="input-3"
+                  id="input-pw2"
                   v-model="form.Password"
                   type="password"
                   required
@@ -116,11 +121,17 @@ export default {
         email: '',
         name: '',
         password: null,
-        checked: []
+        gender: '',
+        checked: [],
+        age: 0
       },
 
       show: true
     }
+  },
+  methods: {
+    onReset: function() {},
+    onSubmit: function() {}
   }
 }
 </script>
