1a28603f052ad7f71209ac3ab3f215af83a23dc6
Davut KARA <davutkara.comtr@yandex.com>
2019-05-03 21:18:48 +0300

    fix(navigation): change icons

--- components/bravadoNavigation.vue
+++ components/bravadoNavigation.vue
@@ -1,22 +1,22 @@
 <template>
   <b-nav id="navigation" tabs justified align="center">
     <b-nav-item active>
-      <b-img src="/icons/svg/009-teamwork.svg" fluid/>
+      <b-img src="/icons/svg/036-employee-2.svg" fluid/>
       <hr>
       <span>Dashboard</span>
     </b-nav-item>
     <b-nav-item>
-      <b-img src="/icons/svg/006-target.svg" fluid/>
+      <b-img src="/icons/svg/042-winner.svg" fluid/>
       <hr>
       <span>My profile</span>
     </b-nav-item>
     <b-nav-item>
-      <b-img src="/icons/svg/022-conversation.svg" fluid/>
+      <b-img src="/icons/svg/003-worlwide.svg" fluid/>
       <hr>
       <span>All challanges</span>
     </b-nav-item>
-    <b-nav-item disabled>
-      <b-img src="/icons/svg/036-employee-2.svg" fluid/>
+    <b-nav-item>
+      <b-img src="/icons/svg/047-employee-5.svg" fluid/>
       <hr>
       <span>Log out</span>
     </b-nav-item>
