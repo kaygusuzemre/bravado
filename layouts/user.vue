05125a3efe79872cd9489ddf467bd765519c5d00
Davut KARA <davutkara.comtr@yandex.com>
2019-05-04 13:32:12 +0300

    feat: add layout for user pages

---
+++ layouts/user.vue
@@ -0,0 +1,96 @@
+<template>
+  <div>
+    <b-row class="justify-content-md-center">
+      <b-col cols="12" md="10">
+        <nuxt/>
+      </b-col>
+    </b-row>
+    <div class="wave mt-5"></div>
+    <footer id="footer">
+      <center>Bravado 2019</center>
+    </footer>
+  </div>
+</template>
+
+<style>
+body {
+  background: #d9e9e9;
+}
+html {
+  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+    Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+
+*,
+*:before,
+*:after {
+  box-sizing: border-box;
+  margin: 0;
+}
+
+.button--green {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #3b8070;
+  color: #3b8070;
+  text-decoration: none;
+  padding: 10px 30px;
+}
+
+.button--green:hover {
+  color: #fff;
+  background-color: #3b8070;
+}
+
+.button--grey {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #35495e;
+  color: #35495e;
+  text-decoration: none;
+  padding: 10px 30px;
+  margin-left: 15px;
+}
+
+.button--grey:hover {
+  color: #fff;
+  background-color: #35495e;
+}
+
+#footer {
+  background: #7dbc9f;
+  color: white;
+  padding: 1em;
+}
+#features .card {
+  border-radius: 50%;
+  text-align: center;
+  padding: 0 5px 10px 5px;
+}
+
+.wave {
+  position: relative;
+}
+.wave::before {
+  content: '';
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  background-repeat: repeat;
+  height: 10px;
+  background-size: 20px 20px;
+  background-image: radial-gradient(
+    circle at 10px -5px,
+    transparent 12px,
+    #7dbc9f 13px
+  );
+}
+</style>
--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,92 +1,91 @@
 <template>
-  <b-row class="justify-content-md-center">
-    <b-col cols="12" md="10">
-      <b-container fluid>
-        <bravado-navigation></bravado-navigation>
-      </b-container>
-      <hr>
-      <b-container id="dashboard" fluid>
-        <b-row>
-          <b-col md="4">
-            <b-card border-variant="light" header="In Progress" class="text-center">
-              <b-dropdown variant="outline-dark">
-                <template slot="button-content">
-                  Sort
-                  <strong>progresses</strong>
-                </template>
-                <b-dropdown-item href="#">Time remain</b-dropdown-item>
-                <b-dropdown-item href="#">Assignment remain</b-dropdown-item>
-              </b-dropdown>
-              <hr>
-              <b-list-group>
-                <b-list-group-item
-                  href="#some-link"
-                  v-for="(ob,i) in [0,1,2,3,4,5,6,7,8,9,10]"
-                  :key="i"
-                >
-                  Reading Challange {{i}} days.
-                  <b-progress :value="100-i*10" :max="100" show-progress animated></b-progress>
-                </b-list-group-item>
-              </b-list-group>
-            </b-card>
-          </b-col>
-          <b-col>
-            <b-card border-variant="light" header="Search a challange" class="text-center">
-              <b-form inline>
-                <b-input placeholder="Write keywords then enter" class="w-100"></b-input>
-              </b-form>
-            </b-card>
+  <div>
+    <b-container fluid>
+      <bravado-navigation></bravado-navigation>
+    </b-container>
+    <hr>
+    <b-container id="dashboard" fluid>
+      <b-row>
+        <b-col md="4">
+          <b-card border-variant="light" header="In Progress" class="text-center">
+            <b-dropdown variant="outline-dark">
+              <template slot="button-content">
+                Sort
+                <strong>progresses</strong>
+              </template>
+              <b-dropdown-item href="#">Time remain</b-dropdown-item>
+              <b-dropdown-item href="#">Assignment remain</b-dropdown-item>
+            </b-dropdown>
             <hr>
-            <b-card border-variant="light" header="Feeds">
-              <ul class="list-unstyled">
-                <b-media tag="li">
-                  <b-img slot="aside" blank blank-color="#abc" width="64" alt="placeholder"></b-img>
+            <b-list-group>
+              <b-list-group-item
+                href="#some-link"
+                v-for="(ob,i) in [0,1,2,3,4,5,6,7,8,9,10]"
+                :key="i"
+              >
+                Reading Challange {{i}} days.
+                <b-progress :value="100-i*10" :max="100" show-progress animated></b-progress>
+              </b-list-group-item>
+            </b-list-group>
+          </b-card>
+        </b-col>
+        <b-col>
+          <b-card border-variant="light" header="Search a challange" class="text-center">
+            <b-form inline>
+              <b-input placeholder="Write keywords then enter" class="w-100"></b-input>
+            </b-form>
+          </b-card>
+          <hr>
+          <b-card border-variant="light" header="Feeds">
+            <ul class="list-unstyled">
+              <b-media tag="li">
+                <b-img slot="aside" blank blank-color="#abc" width="64" alt="placeholder"></b-img>
 
-                  <h5 class="mt-0 mb-1">List-based media object</h5>
-                  <p class="mb-0">
-                    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin.
-                    Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc
-                    ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-                  </p>
-                </b-media>
+                <h5 class="mt-0 mb-1">List-based media object</h5>
+                <p class="mb-0">
+                  Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin.
+                  Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc
+                  ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                </p>
+              </b-media>
 
-                <b-media tag="li" class="my-4">
-                  <b-img slot="aside" blank blank-color="#cba" width="64" alt="placeholder"></b-img>
+              <b-media tag="li" class="my-4">
+                <b-img slot="aside" blank blank-color="#cba" width="64" alt="placeholder"></b-img>
 
-                  <h5 class="mt-0 mb-1">List-based media object</h5>
-                  <p class="mb-0">
-                    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin.
-                    Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc
-                    ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-                  </p>
-                </b-media>
+                <h5 class="mt-0 mb-1">List-based media object</h5>
+                <p class="mb-0">
+                  Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin.
+                  Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc
+                  ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                </p>
+              </b-media>
 
-                <b-media tag="li">
-                  <b-img slot="aside" blank blank-color="#bac" width="64" alt="placeholder"></b-img>
+              <b-media tag="li">
+                <b-img slot="aside" blank blank-color="#bac" width="64" alt="placeholder"></b-img>
 
-                  <h5 class="mt-0 mb-1">List-based media object</h5>
-                  <p class="mb-0">
-                    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin.
-                    Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc
-                    ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-                  </p>
-                </b-media>
-              </ul>
-              <div class="overflow-auto">
-                <b-pagination-nav :number-of-pages="10" use-router></b-pagination-nav>
-              </div>
-            </b-card>
-          </b-col>
-        </b-row>
-      </b-container>
-    </b-col>
-  </b-row>
+                <h5 class="mt-0 mb-1">List-based media object</h5>
+                <p class="mb-0">
+                  Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin.
+                  Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc
+                  ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                </p>
+              </b-media>
+            </ul>
+            <div class="overflow-auto">
+              <b-pagination-nav :number-of-pages="10" use-router></b-pagination-nav>
+            </div>
+          </b-card>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
 </template>
 
 <script>
 import bravadoNavigation from '~/components/bravadoNavigation.vue'
 
 export default {
+  layout: 'user',
   components: { bravadoNavigation }
 }
 </script>
