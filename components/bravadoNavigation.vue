e0e9b987e9ae4015ac61c9481a14433cc3784294
Davut KARA <davutkara.comtr@yandex.com>
2019-05-04 13:59:25 +0300

    fea(navigation): add links

--- components/bravadoNavigation.vue
+++ components/bravadoNavigation.vue
@@ -1,21 +1,22 @@
 <template>
   <b-nav id="navigation" tabs justified align="center">
-    <b-nav-item active>
+    <b-nav-item href="/dashboard" active>
       <b-img src="/icons/svg/036-employee-2.svg" fluid/>
       <hr>
       <span>Dashboard</span>
     </b-nav-item>
-    <b-nav-item>
+
+    <b-nav-item href="/profile">
       <b-img src="/icons/svg/042-winner.svg" fluid/>
       <hr>
       <span>My profile</span>
     </b-nav-item>
-    <b-nav-item>
+    <b-nav-item href="/challanges">
       <b-img src="/icons/svg/003-worlwide.svg" fluid/>
       <hr>
       <span>All challanges</span>
     </b-nav-item>
-    <b-nav-item>
+    <b-nav-item @click="logout()">
       <b-img src="/icons/svg/047-employee-5.svg" fluid/>
       <hr>
       <span>Log out</span>
@@ -24,7 +25,13 @@
 </template>
 
 <script>
-export default {}
+export default {
+  methods: {
+    logout: function() {
+      this.$router.push({ path: '/' })
+    }
+  }
+}
 </script>
 
 <style>
