8b03a58ba69c0ed20c28ded572fc27597e63cc45
darkould <ahmetdogukancitak@gmail.com>
2019-05-14 04:11:13 +0300

    Register form api (#33)
    * Set theme jekyll-theme-midnight
    
    * registerv2
    
    * register form api
    
    * register form api + register form
    
    * register form api

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,24 +1,29 @@
-import validator from 'validator';
+import validator from "validator";
 
-export default function(router) {
-  router.post('/login', (req, res) => {
+export default function (router) {
+  router.post("/auth/login", (req, res) => {
     // Login function
-  })
+  });
 
-  router.post('/logout', (req, res) => {
-    delete req.session.authUser
-    res.json({
-      ok: true
-    })
+  router.post("/auth/logout", (req, res) => {
+    delete req.session.authUser;
+    res.json({ok: true});
     // Logout function
-  })
+  });
 
-  router.post('/register', (req, res) => {
-    if (!validator.isEmail(req.body.email))
-    res.json({
-      status: 'error',
-      key: 'email',
-      msg: 'Email is not valid'
-    })
-  })
+  router.post("/auth/register", (req, res) => {
+    if (!validator.isEmail(req.body.email)) 
+      res.json({status: "error", key: "email", msg: "Email is not valid"});
+    else if (!(req.body.password === req.body.password2)) 
+      res.json({status: "error", key: "password", msg: "Passwords do not match"});
+    else if (!validator.matches(String(req.body.password), /(?=.*[0-9])/)) 
+      res.json({status: "error", key: "password", msg: "The password must contain at least 1 numeric character"});
+    else if (!(req.body.gender === "male" || req.body.gender === "female")) 
+      res.json({status: "error", key: "gender", msg: "gender is needed"});
+    else if (!validator.matches(String(req.body.firstName), /^[a-zA-Z]+$/)) 
+      res.json({status: "error", key: "", msg: "A name can contain only Alpha Characters"});
+    else if (!validator.matches(String(req.body.lastName), /^[a-zA-Z]+$/)) 
+      res.json({status: "error", key: "", msg: "A surname can contain only Alpha Characters"});
+    res.json({status: "success", msg: "success"});
+  });
 }
--- pages/register.vue
+++ pages/register.vue
@@ -6,59 +6,41 @@
           <div>
             <h2>Register</h2>
             <b-form @submit="onSubmit">
-              
               <div class="form-group">
                 <label for="firstName">First Name</label>
-                <input type="text" v-model="user.firstName" v-validate="'required'" id="firstName" name="firstName" class="form-control" :class="{ 'is-invalid': submitted && errors.has('firstName') }" />
-                <div v-if="submitted && errors.has('firstName')" class="invalid-feedback">
-                  {{ errors.first("firstName") }}
-                </div>
+                <input type="text" v-model="user.firstName" id="firstName" class="form-control">
               </div>
               <div class="form-group">
                 <label for="lastName">Last Name</label>
-                <input type="text" v-model="user.lastName" v-validate="'required'" id="lastName" name="lastName" class="form-control" :class="{ 'is-invalid': submitted && errors.has('lastName') }" />
-                <div v-if="submitted && errors.has('lastName')" class="invalid-feedback">
-                  {{ errors.first("lastName") }}
-                </div>
+                <input type="text" v-model="user.lastName" id="lastName" class="form-control">
               </div>
               <div class="form-group">
                 <label for="email">Email</label>
-                <input type="email" v-model="user.email" v-validate="'required|email'" id="email" name="email" class="form-control" :class="{ 'is-invalid': submitted && errors.has('email') }" />
-                <div v-if="submitted && errors.has('email')" class="invalid-feedback">
-                  {{ errors.first("email") }}
-                </div>
+                <input type="email" v-model="user.email" id="email" class="form-control">
               </div>
               <b-form-group id="input-group-2" label="Age" label-for="input-2">
-        <b-form-input
-          id="input-2"
-          type="number"
-          v-model="user.age"
-          
-        ></b-form-input>
-      </b-form-group>
+                <b-form-input id="input-2" type="number" v-model="user.age"></b-form-input>
+              </b-form-group>
 
-<b-form-group label="Gender">
-        <b-form-radio v-model="user.gender" name="some-radios" value="male">Male</b-form-radio>
-        <b-form-radio v-model="user.gender" name="some-radios" value="female">Female</b-form-radio>
-      </b-form-group>
+              <b-form-group label="Gender">
+                <b-form-radio v-model="user.gender" name="some-radios" value="male">Male</b-form-radio>
+                <b-form-radio v-model="user.gender" name="some-radios" value="female">Female</b-form-radio>
+              </b-form-group>
 
               <div class="form-group">
                 <label for="password">Password</label>
-                <input type="password" v-model="user.password" v-validate="{ required: true, min: 6 }" id="password" name="password" class="form-control" :class="{ 'is-invalid': submitted && errors.has('password') }" />
-                <div v-if="submitted && errors.has('password')" class="invalid-feedback">
-                  {{ errors.first("password") }}
-                </div>
-
-                <div class="form-group">
-                <label for="password2">Password again</label>
-                <input type="password2" v-model="user.password2" v-validate="{ required: true, min: 6 }" id="password2" name="password2" class="form-control" :class="{ 'is-invalid': submitted && errors.has('password2') }" />
-                <div v-if="submitted && errors.has('password2')" class="invalid-feedback">
-                  {{ errors.first("password2") }}
-                </div>
-                </div>
+                <input type="password" v-model="user.password" id="password" class="form-control">
               </div>
-             <b-button variant="outline-info"><b-link to="/login" >Do You Have an Account</b-link> </b-button>
-             <b-button type="submit" variant="success">Register</b-button>
+
+              <div class="form-group">
+                <label for="password2">Password again</label>
+                <input type="password" v-model="user.password2" id="password2" class="form-control">
+              </div>
+              <b-button variant="outline-info">
+                <b-link to="/login">Do You Have an Account</b-link>
+              </b-button>
+              <b-button type="submit" variant="success">Register</b-button>
+              {{reqMsg}}
             </b-form>
           </div>
         </div>
@@ -76,23 +58,26 @@ export default {
         firstName: "",
         lastName: "",
         email: "",
-        age:"",
-        gender:"",
+        age: "",
+        gender: "",
         password: "",
-        password2:""
+        password2: ""
       },
-      result: null
+      result: null,
+      reqMsg: null
     };
   },
-  
+
   methods: {
-        async onSubmit(e) {
-          e.preventDefault()
-      this.submitted = true;
-      alert(JSON.stringify(this.user))
-      const req = await this.$axios.$post('/api/routes/auth', this.user)
-      
-      }
+    async onSubmit(e) {
+      e.preventDefault();
+      const req = await this.$axios.$post("/api/auth/register", this.user);
+
+      console.log(req.msg);
+      if (req.status === "error" || req.status === "success")
+        this.reqMsg = req.msg;
+      else this.errmsg = null;
+    }
   }
 };
 </script>
