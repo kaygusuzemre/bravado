f1a691ab5737c4f288e7ba0731c769b1066caa98
Davut KARA <davutkara.comtr@yandex.com>
2019-05-17 17:22:30 +0300

    fix(auth): fix crash error

--- api/index.js
+++ api/index.js
@@ -13,6 +13,18 @@ db.connect()
 db.on('error', function() {})
 const router = express.Router()
 const app = express()
+
+if (process.env.NODE_ENV === 'development') {
+  router.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept'
+    )
+    next()
+  })
+}
+
 router.use((req, res, next) => {
   Object.setPrototypeOf(req, app.request)
   Object.setPrototypeOf(res, app.response)
