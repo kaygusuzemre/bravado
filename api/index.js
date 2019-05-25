09097371d107e3e3f4613ec82196b492d1c7f5f3
dogukan citak <ahmetdogukancitak@gmail.com>
2019-05-25 20:02:09 +0300

    change account settings form validation(unknown error)

--- api/index.js
+++ api/index.js
@@ -1,42 +1,33 @@
-import express from 'express'
-import mysql from 'mysql'
-import auth from './routes/auth'
-import redis from 'redis'
-const cache = redis.createClient()
-const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'example',
-  database: 'bravado'
-})
+import express from "express";
+import mysql from "mysql";
+import auth from "./routes/auth";
+import user from "./routes/user";
+import redis from "redis";
+const cache = redis.createClient();
+const db = mysql.createConnection({host: "localhost", user: "root", password: "example", database: "bravado"});
 
-db.connect()
-db.on('error', function() {})
-const router = express.Router()
-const app = express()
+db.connect();
+db.on("error", function () {});
+const router = express.Router();
+const app = express();
 
-if (process.env.NODE_ENV === 'development') {
-  router.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept'
-    )
-    next()
-  })
+if (process.env.NODE_ENV === "development") {
+  router.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 }
 
 router.use((req, res, next) => {
-  Object.setPrototypeOf(req, app.request)
-  Object.setPrototypeOf(res, app.response)
-  req.res = res
-  res.req = req
-  next()
-})
+  Object.setPrototypeOf(req, app.request);
+  Object.setPrototypeOf(res, app.response);
+  req.res = res;
+  res.req = req;
+  next();
+});
 
-auth(router, db, cache)
+auth(router, db, cache);
+user(router, db, cache);
 
-export default {
-  path: '/api',
-  handler: router
-}
+export default {path: "/api", handler: router};
