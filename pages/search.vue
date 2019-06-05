3f5818f4aa82578bf1a8b72df6844cc461c8e661
gulocak <aliefegulocak@gmail.com>
2019-06-05 22:42:16 +0300

    search page added

---
+++ pages/search.vue
@@ -0,0 +1,82 @@
+<template>
+<div>
+    <b-container fluid>
+    <bravado-navigation></bravado-navigation>
+    </b-container>
+    ,<hr>
+    <b-container id="list" fluid>
+        <b-row>
+            <b-col md="5">
+                <div id="choose" class="p-2 mb-2 bg-primary text-white">Choose Categories</div>
+                </b-col>
+                <b-col md="4">
+                    <b-form-input size="lg" class="mr-sm-2" placeholder="Write a challenge topic"></b-form-input>
+
+                </b-col>
+                <b-col md="3">
+                   <b-button block size="lg"  variant="primary">Search a challenge</b-button>
+                  
+                    </b-col>
+                    <b-col  md="5">
+<b-form-select v-model="selected" :options="options" :select-size="20"></b-form-select>
+                <div class="mt-3">Selected: <strong>{{ selected }}</strong></div>
+
+
+    </b-col>
+                    <b-col  md="3">
+    <div id="assignment" class="p-2 mb-2 bg-primary text-white">Results</div>
+    </b-col>
+    
+                    
+                    
+
+        </b-row>
+        
+    </b-container>
+
+</div>
+</template>
+<script>
+import bravadoNavigation from '~/components/bravadoNavigation.vue'
+export default {
+layout: 'user',
+ components: { bravadoNavigation },
+  data() {
+      return {
+        selected: null,
+        options: [
+          { value: 'a', text: 'Item 1' },
+          { value: 'b', text: 'Item 2' },
+          { value: 'c', text: 'Item 3' },
+          { value: 'd', text: 'Item 4', },
+          { value: 'e', text: 'Item 5' },
+          { value: 'f', text: 'Item 6' },
+          { value: 'g', text: 'Item 6' },
+          { value: 'h', text: 'Item 7' },
+          { value: 'i', text: 'Item 8' },
+          { value: 'j', text: 'Item 9', },
+          { value: 'k', text: 'Item 10' }
+         
+        ]
+      }
+    }
+}
+
+   
+</script>
+<style>
+#choose{
+    font-size:25px;
+    text-align: center;
+    color:rgb(0, 255, 221);
+
+}
+#assignment{
+  font-size: 25px;
+text-align: center;
+
+ 
+}
+
+
+</style>
